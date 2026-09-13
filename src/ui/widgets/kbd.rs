7f4ca38853a0d943b5afc5df98ab721409a0256a
emilhals <emilhals.i@gmail.com>
2026-09-13 15:28:11 +0200

    refactor(ui): split Content-property into Key & Content-property.
    Use String instead of Text for both properties.

--- src/ui/widgets/kbd.rs
+++ src/ui/widgets/kbd.rs
@@ -2,27 +2,35 @@ use derive_setters::Setters;
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
-    style::Style,
-    text::{Line, Text},
-    widgets::{Block, BorderType, Borders, Clear, Paragraph, Widget, Wrap},
+    style::{Color, Style},
+    text::{Line, Span, Text},
+    widgets::{Block, Clear, Paragraph, Widget},
 };
 
 #[derive(Debug, Default, Setters)]
-pub struct Kbd<'a> {
-    #[setters(into)]
-    content: Text<'a>,
+pub struct Kbd {
+    key: String,
+    content: String,
 }
 
-impl Widget for Kbd<'_> {
+impl Widget for Kbd {
     fn render(self, area: Rect, buf: &mut Buffer) {
         Clear.render(area, buf);
 
+        let mut lines = vec![];
+        lines.push(Line::from(vec![
+            Span::styled("_", Style::default().bg(Color::Gray)),
+            Span::styled(
+                format!("{:^2}", self.key),
+                Style::default().italic().fg(Color::Black).bg(Color::Gray),
+            ),
+            Span::styled(" ", Style::default()),
+            Span::styled(self.content, Style::default().fg(Color::Gray)),
+        ]));
+
+        let text = Text::from(lines);
+
         let block = Block::new();
-        Paragraph::new(self.content)
-            .wrap(Wrap { trim: true })
-            .style(Style::new().on_blue())
-            .centered()
-            .block(block)
-            .render(area, buf);
+        Paragraph::new(text).block(block).render(area, buf);
     }
 }
